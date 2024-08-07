4e4b148bde421455369a622d5050f2080e19427c
abhaygowdamk <abhaygowda136@gmail.com>
2024-08-07 22:44:15 +0530

    User registration screen file

---
+++ register_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'register_form.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade400, Colors.blue.shade900],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 120.0),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: RegisterForm(),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
